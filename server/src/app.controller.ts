69c5d701cb8a5413e8b421304a466e4c37ed8c6d
Nicolás Breyaui <nbreyaui@apn.gob.ar>
2026-06-12 14:39:36 -0300

    modulos nest

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,19 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
     return this.appService.getHello();
+  }
+
+  @Get(':id')
+  getById(@Param('id') param: string): any {
+    return param;
+  }
+
+  @Post()
+  create(@Body() body: any): any {
+    return {
+      message: body,
+    };
   }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioController } from './usuario/usuario.controller';
+import { UsuariosController } from './usuarios/usuarios.controller';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsuariosModule],
+  controllers: [AppController, UsuarioController, UsuariosController],
   providers: [AppService],
 })
 export class AppModule {}
